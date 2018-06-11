3e7d3d0057f7c7c99d6849a53a19b24c12743820
unknown <g031o163@ad.iwate-pu.ac.jp>
2018-06-11 12:17:48 +0900

    fizzbuzz

---
+++ FizzBuzz.php
@@ -0,0 +1,14 @@
+<?php
+  for($i = 1;$i <= 100; $i++){
+    if ($i%15 == 0){
+       echo 'buzzfizz →';
+    }elseif($i%3 == 0){
+      echo 'buzz → ';
+    }elseif($i%5 == 0){
+      echo 'fizz → ';
+    }
+    else{
+      echo "$i →";
+    }
+  }
+?>
