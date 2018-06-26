f5222405e9ef8c5a52d3212ae27dab02867a13fa
unknown <g031o163@ad.iwate-pu.ac.jp>
2018-06-26 17:59:55 +0900

    入力強制・NULLなし

--- form.php
+++ form.php
@@ -9,9 +9,9 @@
     <h1>ユーザ登録</h1>
     <form method = "POST" action = "record.php">
       <!-- record.phpに渡す -->
-        ユーザ名:<input type="text" name="User_name"size="20"maxlength="15"><br><br>
+        ユーザ名:<input type="text" name="User_name"size="20"maxlength="15" required><br><br>
         <!-- ユーザー名入力 -->
-        パスワード:<input type="text" name="password"size="20"maxlength="15"><br><br>
+        パスワード:<input type="text" name="password"size="20"maxlength="15" required><br><br>
         <!-- パスワード入力 -->
         <input type = "submit" value = "登録" span style="color:#ff0000;"/>
         <!-- 送信ボタン -->
--- record.php
+++ record.php
@@ -6,39 +6,44 @@
 </head>
 <body>
   <center>
-  <h1>登録結果</h1>
-</body>
-<table border="1">
-  <tr><th>id</th><th>名前</th><th>パスワード</th></tr>
-  <!-- 表式決定 -->
-</center>
+    <h1>登録結果</h1>
+  </body>
+  <table border="1">
+    <tr><th>id</th><th>名前</th><th>パスワード</th></tr>
+    <!-- 表式決定 -->
+  </center>
 
-<?php
-$User_name=htmlspecialchars($_POST['User_name']);
-// ユーザー名受け取り
-$password=htmlspecialchars($_POST['password']);
-// パスワード受け取り
-try{
-  $pdo = new PDO('mysql:host=localhost;dbname=g031o163;charset=utf8','g031o163','uJqY5ht6wacvacip',
-  // サーバー情報
-  array(PDO::ATTR_EMULATE_PREPARES => false));
-  $stmt = $pdo -> prepare("INSERT INTO user (User_name,password) VALUES (:User_name,:password) ");
-  // データ挿入
-  $stmt->bindParam(':User_name',$User_name, PDO::PARAM_STR);
-  $stmt->bindParam(':password',$password, PDO::PARAM_STR);
-  $stmt->execute();
-  $dbh = null;
-  $stmt = $pdo->query("SELECT * FROM `user`");
-  // データ選択
-  while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-    echo '<tr>';
-    foreach($row as $key){
-      echo '<td>'.$key.'</td>';
+  <?php
+  try{
+    $pdo = new PDO('mysql:host=localhost;dbname=g031o163;charset=utf8','g031o163','uJqY5ht6wacvacip',
+    // サーバー情報
+    array(PDO::ATTR_EMULATE_PREPARES => false));
+    if ($_POST != null) {
+      // nullでなければ
+      $User_name=htmlspecialchars($_POST['User_name']);
+      // ユーザー名受け取り
+      $password=htmlspecialchars($_POST['password']);
+      // パスワード受け取り
+
+
+      $stmt = $pdo -> prepare("INSERT INTO user (User_name,password) VALUES (:User_name,:password) ");
+      // データ挿入
+      $stmt->bindParam(':User_name',$User_name, PDO::PARAM_STR);
+      $stmt->bindParam(':password',$password, PDO::PARAM_STR);
+      $stmt->execute();
+      $dbh = null;
     }
-    echo '</tr>';
+    $stmt = $pdo->query("SELECT * FROM `user`");
+    // データ選択
+    while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+      echo '<tr>';
+      foreach($row as $key){
+        echo '<td>'.$key.'</td>';
+      }
+      echo '</tr>';
+    }
+    // データ表示
+  } catch (PDOException $e) {
+    exit('データベース接続失敗。'.$e->getMessage());
   }
-  // データ表示
-} catch (PDOException $e) {
-  exit('データベース接続失敗。'.$e->getMessage());
-}
-?>
+  ?>
